6ce61ee20b628958898874dae26fd2829aae9f2f
Thiago Ozores <tozores@gmail.com>
2021-03-04 15:55:07 -0300

    versao 1.0

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 import socket
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "Hello from {}!\n".format(socket.gethostname())
+    data = {
+        'hostname': socket.gethostname(),
+        'version': '1.0'
+    }
+    return render_template('hello.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
