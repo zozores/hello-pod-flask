9434788ee4cce5d62e5ee3381be349e6c9a284e0
Thiago Ozores <tozores@gmail.com>
2021-03-04 15:59:03 -0300

    subindo versão 2.0

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello():
     data = {
         'hostname': socket.gethostname(),
-        'version': '1.0'
+        'version': '2.0'
     }
     return render_template('hello.html', data=data)
 
